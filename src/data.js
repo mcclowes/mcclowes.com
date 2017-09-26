6b706a47762cd4376aa26d149e9900c9612dbdee
Josh Dronsfield <josh.dronsfield@gmail.com>
2017-09-26 17:51:57 +0100

    init

---
+++ src/data.js
@@ -0,0 +1,89 @@
+import _slugify from "slugify";
+import marked from "marked";
+
+import rawdata from "./rawdata";
+
+const slugify = x => _slugify(x, {
+	lower: true,
+});
+
+// transform a field or do something to an existing field to add a new one
+const adjustFields = (a, b, fn) => fieldsObj => ({
+	...fieldsObj,
+	...(
+		fieldsObj[a]
+		? { [b]: fn(fieldsObj[a]), }
+		: {}
+	),
+});
+
+// transform an array of objects to a map of objects, where the keys are object.slug
+const makeMapUsingSlugs = list => {
+	return list.reduce((acc, item) => (
+		{
+			...acc,
+			[item.slug]: item,
+		}
+	), {} )
+};
+
+const shapeImageField = o => {
+	if (o.fields) {
+		const {
+			fields: {
+				file: {
+					url,
+					details: {
+						size,
+						image: {
+							width,
+							height,
+						},
+					},
+					fileName,
+					contentType,
+				},
+			},
+		} = o;
+
+		return {
+			contentType,
+			fileName,
+			height,
+			size,
+			url,
+			width,
+		};
+	}
+	else {
+		return o;
+	}
+};
+
+const defaultFieldShaping = R.pipe(
+	adjustFields("image", "image", shapeImageField),
+	adjustFields("title", "slug", slugify),
+	adjustFields("content", "html", marked),
+);
+
+const dataObj = {};
+rawdata.items.forEach(item => {
+	const itemType = item.sys.contentType.sys.id;
+	const shapedItem = {
+		...defaultFieldShaping(item.fields),
+		createdAt: item.sys.createdAt,
+	};
+
+	dataObj[itemType] = (
+		dataObj[itemType]
+		? dataObj[itemType].concat(shapedItem)
+		: [ shapedItem, ]
+	);
+});
+
+// --------------------------------------------------
+
+// shape the data however you want my guy
+const retval = dataObj;
+
+export default retval;
