ceccc955e361a58c876b05b5ea3a17527c9d07ca
Freddie Ridell <freddie@codogo.io>
2018-01-17 10:28:04 +0000

    fix(netlify): snapshot just doesn't work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { render, } from "react-snapshot";
 
 render(<App />, document.getElementById("root"));
-registerServiceWorker();
+
+navigator.serviceWorker.getRegistrations().then((registrations) => {
+	for(let registration of registrations) {
+		registration.unregister()
+	}
+})
