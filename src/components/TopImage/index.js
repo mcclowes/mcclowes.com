14027f39780a72ff46b1e281bb42d667a4110585
Maxi Clayton Clowes <maxi@codogo.io>
2018-06-13 11:19:02 +0100

    feat(new): Gatsby baby

---
+++ src/components/TopImage/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+import { Image, } from "semantic-ui-react";
+
+// ----------------------------
+
+export const TopImage = styled(Image)`
+	width: 100%;
+	height: 400px;
+	object-fit: cover;
+	z-index: -1;
+`;
