a4e6722a24cb9d3182b3862517f0a44c8c32c26e
Maxi Clayton Clowes <maxi@codogo.io>
2018-06-25 12:07:58 +0100

    chore(gitignore): cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/mcclowes/Development/mcclowes.com/src/layouts/index.js"
-  import data from "/Users/mcclowes/Development/mcclowes.com/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
