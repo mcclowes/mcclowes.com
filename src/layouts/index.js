1955d2a1cfb0a57dd7b3c4fd63309478d652458d
mcclowes <maxcc@me.com>
2019-03-18 16:43:51 +0000

    using custom again

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,14 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-
 import Header from "../components/header";
-import "./index.css";
+import Helmet from "react-helmet";
+import PropTypes from "prop-types";
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { graphql } from 'gatsby'
+
+const Page = styled.div`
+	width: 100%;
+	height: 100%;
+`;
 
 // ----------------------------------
 
@@ -17,10 +22,17 @@ export const query = graphql`
 	}
 `;
 
+const GlobalStyle = createGlobalStyle`
+	body {
+		margin: 0;
+		background-color: black;
+	}
+`;
+
 // ----------------------------------
 
 const Layout = ({ children, data, }) => (
-	<div>
+	<Page>
 		<Helmet
 			title = { data.site.siteMetadata.title }
 			meta = { [
@@ -30,17 +42,10 @@ const Layout = ({ children, data, }) => (
 			] }
 		/>
 
-		<div
-			style = { {
-				margin: "0 auto",
-				maxWidth: 960,
-				padding: "0px 1.0875rem 1.45rem",
-				paddingTop: 0,
-			} }
-		>
-			{ children() }
-		</div>
-	</div>
+		<GlobalStyle/>
+		
+		{ children() }
+	</Page>
 );
 
 Layout.propTypes = {
