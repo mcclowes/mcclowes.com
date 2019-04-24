68bc6979c405afdabe81ca816e3705c7b2647e29
mcclowes <maxcc@me.com>
2019-04-24 12:32:26 +0100

    [feat] webring links, add layout

---
+++ src/components/footer.js
@@ -0,0 +1,47 @@
+import Link from "gatsby-link";
+import PropTypes from "prop-types";
+import React from "react";
+import styled from "styled-components";
+
+const FooterLinks = styled.div`
+  position: absolute;
+  bottom: 0;
+  padding: 1rem 0;
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-family: helvetica;
+    font-weight: bolder;
+    letter-spacing: 0.3rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 1.2rem;
+
+    &,
+    &:hover,
+    &:active {
+      -webkit-text-stroke-width: 1px;
+      color: transparent;
+      -webkit-text-stroke-color: #DDA1B7;
+    }
+
+    &:hover { opacity: 0.75; }
+    &:active { opacity: 0.5; }
+  }
+`;
+
+const Footer = ({ siteTitle, }) => (
+	<FooterLinks>
+    <a href="https://personal-sites-web-ring.freddieridell.now.sh/prev">⬅️</a>
+    <a href="https://personal-sites-web-ring.freddieridell.now.sh/home">Webring</a>
+    <a href="https://personal-sites-web-ring.freddieridell.now.sh/next">➡️</a>
+    <a href="https://personal-sites-web-ring.freddieridell.now.sh/rand">🔀</a>
+  </FooterLinks>
+);
+
+Footer.propTypes = {
+	siteTitle: PropTypes.any,
+};
+
+export default Footer;
