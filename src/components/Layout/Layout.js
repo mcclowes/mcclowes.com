caa6a4dbcbe0b8a19091ecbbdddae461f3390ba9
Max Clayton Clowes <maxcc@me.com>
2019-11-05 15:42:45 +0000

    [feat] restructure

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
-import Footer from "../components/footer";
-import Header from "../components/header";
+import Footer from "../Footer";
+import Header from "../Header";
 import Helmet from "react-helmet";
 import PropTypes from "prop-types";
 import React from "react";
