889d871ec836467e220bc9b388075afc643765a9
Max Clayton Clowes <maxcc@me.com>
2020-03-09 21:20:03 +0000

    [fix] gatsby build and babel config

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,11 +1,12 @@
+import Layout from "../../components/Layout";
 import React from "react";
 
 const NotFoundPage = () => (
-  <div>
+  <Layout>
     <h1>NOT FOUND</h1>
 
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  </Layout>
 );
 
 export default NotFoundPage;
