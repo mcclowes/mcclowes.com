4a074609ce7bed2be41f4eec2d80874a36088b27
Max Clayton Clowes <maxcc@me.com>
2020-03-10 11:14:35 +0000

    [feat] new design

--- src/components/UIKit/Text/csx.js
+++ src/components/UIKit/Text/csx.js
@@ -1,28 +1,30 @@
 import styled from "styled-components/macro";
 
 export const Header = styled.h1`
-  color: ${props => props.theme.colors.black};
+  color: ${props => props.theme.colors.text};
   font-size: 1.2em;
   font-family: Helventica, Arial, sans-serif;
+  margin: 0;
 `;
 
 Header.defaultProps = {
   theme: {
     colors: {
-      black: "#333333"
+      text: "#333333"
     }
   }
 };
 
 export const Paragraph = styled.p`
-  color: ${props => props.theme.colors.black};
+  color: ${props => props.theme.colors.text};
   font-family: Helventica, Arial, sans-serif;
+  margin: 0;
 `;
 
 Paragraph.defaultProps = {
   theme: {
     colors: {
-      black: "#333333"
+      text: "#333333"
     }
   }
 };
@@ -30,14 +32,15 @@ Paragraph.defaultProps = {
 export const Meta = styled.p`
   margin: 0;
   font-size: 0.8em;
-  color: ${props => props.theme.colors.gray};
+  color: ${props => props.theme.colors.meta};
   font-family: Helventica, Arial, sans-serif;
+  margin: 0;
 `;
 
 Meta.defaultProps = {
   theme: {
     colors: {
-      gray: "#aaaaaa"
+      meta: "#aaaaaa"
     }
   }
 };
