0da442cd4423649aaf96273f91b8b96571c46ee4
Max Clayton Clowes <max.clayton-clowes@pollen.co>
2020-10-27 16:19:32 +0000

    [feat] styled components and other deps

--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,71 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import { fetchEntries } from '@utils/contentfulPosts'
+import { fetchEntries } from "@utils/contentfulPosts";
 
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import Post from '@components/Post'
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import Post from "@components/Post";
+
+import styled from "styled-components";
+
+import { GlobalStyle } from "../utils/styles/globalStyles";
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  background-color: #333;
+`;
+
+const Posts = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Home = ({ posts }) => {
   return (
-    <div className="container">
+    <Container>
+      <GlobalStyle />
+
       <Head>
         <title>mcclowes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <Main>
         <Header />
-        <div className="posts">
+
+        <Posts>
           {posts.map((post, i) => {
-            return <Post key={i} {...post} />
+            return <Post key={i} {...post} />;
           })}
-        </div>
-      </main>
+        </Posts>
+      </Main>
 
       <Footer />
+    </Container>
+  );
+};
 
-      <style jsx>{`
-        .container {
-          height: 100vh;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .posts {
-          display: flex;
-          flex-direction: column;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
 
 export const getStaticProps = async () => {
-  const res = await fetchEntries()
+  const res = await fetchEntries();
 
   // normalize
   const posts = await res.map((post) => {
-    return post.fields
-  })
+    return post.fields;
+  });
 
   return {
     props: {
       posts,
     },
-  }
-}
+  };
+};
