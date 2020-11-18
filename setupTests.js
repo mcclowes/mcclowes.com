125972c29ea54a756fe61ed21745ff4b17ec8d6c
Max Clayton Clowes <max.clayton-clowes@pollen.co>
2020-11-18 22:18:21 +0000

    [fix] screenshots and jest setup

---
+++ setupTests.js
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom/extend-expect";
+
+import "jest-styled-components";
+
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
