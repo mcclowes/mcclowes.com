6a3555f5bfe82f810ccce2bc863f5a640ed23f40
Max Clayton Clowes <max.clayton-clowes@pollen.co>
2020-11-18 18:26:42 +0000

    [feat] chromatic

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    "../(components|pages|utils)/**/*.stories.mdx",
+    "../(components|pages|utils)/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
