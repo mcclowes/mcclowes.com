eca77fbcbe07b8a910d96eefab042ccc606368a2
Max Clayton Clowes <max@mcclowes.com>
2022-07-22 10:30:49 +0100

    [fix] titles

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,13 +6,13 @@ const HeaderWrapper = styled.h1`
 
 const Header = () => {
   return <div>
-    <HeaderWrapper className="title">Mike Clowes</HeaderWrapper>
+    <HeaderWrapper className="title">Max Clayton Clowes</HeaderWrapper>
 
     <p>
       <a href="mailto:contact@mcclowes.com">contact@mcclowes.com</a>
     </p>
 
-    {/* <p>I am a Product Manager with a diverse software engineering and design background, and experience as a founder of a client-facing business. I've been delivering websites and apps for 10+ years. Duke of York Young Entrepreneur Award winner 2017.</p> */}
+    <p>I am a Product Manager with a diverse software engineering and design background, and experience as a founder of a client-facing business. I've been delivering websites and apps for 10+ years. Duke of York Young Entrepreneur Award winner 2017.</p>
   </div>
 };
 
