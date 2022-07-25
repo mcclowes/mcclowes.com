a86b5abcd7462e6d5f29cad287d851fb5218c8f4
Max Clayton Clowes <contact@mcclowes.com>
2022-07-25 23:03:27 +0100

    [feat] sort posts and improved styles

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 
-const HeaderWrapper = styled.h1`
-`;
+const HeaderWrapper = styled.h1``;
 
 const Header = () => {
-  return <div>
-    <HeaderWrapper className="title">Max Clayton Clowes</HeaderWrapper>
+  return (
+    <div>
+      <HeaderWrapper className="title">Max Clayton Clowes</HeaderWrapper>
 
-    <p>
-      <a href="mailto:contact@mcclowes.com">contact@mcclowes.com</a>
-    </p>
+      <p>
+        <a href="mailto:contact@mcclowes.com">contact@mcclowes.com</a>
+      </p>
 
-    <p>I am a Product Manager with a diverse software engineering and design background, and experience as a founder of a client-facing business. I've been delivering websites and apps for 10+ years. Duke of York Young Entrepreneur Award winner 2017.</p>
-  </div>
+      <p>
+        I am a Product Manager with a diverse software engineering and design
+        background. I use my background to get the best out of my developement
+        peers, and the best for my developer users. Intuition lead, data backed.
+        I've been delivering websites and apps for 10+ years. DoY's Young
+        Entrepreneur Award winner as a founder.
+      </p>
+    </div>
+  );
 };
 
 export default Header;
--- components/Post/index.js
+++ components/Post/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
+import moment from "moment";
 
 const Container = styled.div`
   display: flex;
@@ -14,7 +15,8 @@ const Text = styled.div`
   padding: 0 1em;
 
   h2,
-  h3 {
+  h3,
+  h4 {
     margin-top: 0;
   }
 `;
@@ -27,35 +29,54 @@ const Image = styled.img`
 `;
 
 const Date = styled.p`
-  opacity: 0.9;
+  opacity: 0.75;
+  margin: 0;
 `;
 
-const Description = styled.p`
+const Description = styled.p``;
 
+const LinkWrapperLink = styled.a`
+  &:hover {
+    text-decoration: none;
+  }
 `;
+
+const LinkWrapper = (props) => {
+  console.log(props);
+
+  return props.externalLink ? (
+    <LinkWrapperLink href={props.externalLink} target="_blank">
+      {props.children}
+    </LinkWrapperLink>
+  ) : (
+    props.children
+  );
+};
 
 const Post = (props) => {
-  const { publishingDate, image, title, description } = props;
+  const { publishingDate, image, title, description, externalLink } = props;
 
   if (!publishingDate) return null;
 
   return (
-    <Container className="post">
-      {image && image.fields ? (
-        <Image
-          alt={image.fields.description}
-          src={`https:${image.fields.file.url}`}
-        />
-      ) : null}
+    <LinkWrapper externalLink={externalLink}>
+      <Container className="post">
+        {image && image.fields ? (
+          <Image
+            alt={image.fields.description}
+            src={`https:${image.fields.file.url}`}
+          />
+        ) : null}
 
-      <Text>
-        <h4>{title}</h4>
+        <Text>
+          <h4>{title}</h4>
 
-        <Date>{publishingDate.substring(0, 10)}</Date>
+          <Date>{moment(publishingDate).format("MMMM Do, YYYY")}</Date>
 
-        <Description>{description}</Description>
-      </Text>
-    </Container>
+          <Description>{description}</Description>
+        </Text>
+      </Container>
+    </LinkWrapper>
   );
 };
 
